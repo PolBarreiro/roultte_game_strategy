3258238cfcc39c5ec7827fd86f42d203294cb439
PolBarreiro <114869830+PolBarreiro@users.noreply.github.com>
2024-01-01 01:57:34 +0100

    Update roulette_game_strategy.py

--- roulette_game_strategy.py
+++ roulette_game_strategy.py
@@ -13,12 +13,12 @@
 
 import random
 
-mon = 5000  # the money you start with
+mon = 50000  # the money you start with
 bw = 0  # the output ball (red or black)
 ap = 1  # the starting bet (one dollar)
 count = 0
 play = 100000  # the number of times you want to play
-max = 8  # the number of times you can lose (1$,3$,9$,27$,81$,243$,729$,2187$,6561,19683...)
+max = 9  # the number of times you can lose (1$,3$,9$,27$,81$,243$,729$,2187$,6561,19683...)
 
 # you can play with numbers above
 # play many times as you need, results very each time
@@ -26,22 +26,24 @@ max = 8  # the number of times you can lose (1$,3$,9$,27$,81$,243$,729$,2187$,65
 # you can even play double instead of triple ***here***
 
 for i in range(play):
-    count = 0
     print(mon)
     while True:
         if mon <= 0:
             print('you loose')
             exit()
         bw = random.randint(1, 2)
-        count = count + 1
         if count < max:
-            if bw == 2:
+            if bw == 1:
                 mon = mon + ap
                 ap = 1
+                count = count + 1
                 break
             else:
                 ap = ap * 3  # ***here***
+                count = count + 1
+                break
         else:
             mon = mon - ap
             ap = 1
+            count = 0
             break
